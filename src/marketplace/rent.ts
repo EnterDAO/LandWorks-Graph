ddbe52d1dcc9b26d3fec467bad234dac0ff27331
failfmi <oscurocalma@gmail.com>
2021-11-29 12:49:26 +0200

    fix(rent): create user if not exists
    Signed-off-by: failfmi <oscurocalma@gmail.com>

--- src/marketplace/rent.ts
+++ src/marketplace/rent.ts
@@ -4,13 +4,15 @@ import { common } from '../common';
 export function handleRent(event: EventRent): void {
   const assetId = event.params._assetId.toString();
   const rentId = `${assetId}-${event.params._rentId.toString()}`;
+  const renter = event.params._renter.toHexString();
 
   const rent = common.createRentIfNotExists(rentId);
   rent.asset = assetId;
   rent.start = event.params._start;
   rent.end = event.params._end;
   rent.fee = event.params._fee;
-  rent.renter = event.params._renter.toHexString();
+  common.createUserIfNotExists(renter);
+  rent.renter = renter;
   rent.save();
   common.assetUpdateLatest(assetId, rent.fee, rent.end);
   common.incrementTotalRents();

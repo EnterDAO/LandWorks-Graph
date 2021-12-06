74445c1e1a9192340b4058a2bb82ba244ab2a89e
failfmi <oscurocalma@gmail.com>
2021-12-06 18:00:19 +0200

    fix: rent payment token & tx hash
    Signed-off-by: failfmi <oscurocalma@gmail.com>

--- src/marketplace/rent.ts
+++ src/marketplace/rent.ts
@@ -5,14 +5,18 @@ export function handleRent(event: EventRent): void {
   const assetId = event.params._assetId.toString();
   const rentId = `${assetId}-${event.params._rentId.toString()}`;
   const renter = event.params._renter.toHexString();
+  const paymentToken = event.params._paymentToken;
+  common.createTokenPaymentIfNotExists(paymentToken);
 
   const rent = common.createRentIfNotExists(rentId);
   rent.asset = assetId;
   rent.start = event.params._start;
   rent.end = event.params._end;
+  rent.paymentToken = paymentToken.toHexString();
   rent.fee = event.params._fee;
   common.createUserIfNotExists(renter);
   rent.renter = renter;
+  rent.txHash = event.transaction.hash.toHexString();
   rent.save();
   common.assetUpdateLatest(assetId, rent.fee, rent.end);
   common.incrementTotalRents();
